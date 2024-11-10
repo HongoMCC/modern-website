8da8fd096140dcb7e08649d06ca8e31ae41893e1
UHAsikakutou <91722200+UHAsikakutou@users.noreply.github.com>
2024-11-10 18:06:03 +0900

    Feat: headings and articles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,25 @@
 import {
   Box,
   Center,
+  Divider,
   Heading,
+  HStack,
   IconButton,
   Spacer,
+  Text,
   VStack,
 } from "@yamada-ui/react";
 import TypeIt from "typeit-react";
 import { JetBrains_Mono } from "next/font/google";
 import Header from "@/components/header";
 import { MdArrowDownward } from "react-icons/md";
+import { useRef } from "react";
 const MonoFont = JetBrains_Mono({
   weight: "variable",
   subsets: ["latin"],
 });
 export default function Home() {
+  const contentRef = useRef<HTMLDivElement>(null);
   const words = ["Hongo.", "myself.", "everything!"];
   return (
     <>
@@ -63,17 +68,89 @@ export default function Home() {
           </Box>
           <Heading textColor="white">本郷をハックしよう。</Heading>
           <Spacer />
-          <Center>
+          <Center ref={contentRef}>
             <IconButton
               icon={<MdArrowDownward />}
               size="lg"
               color="white"
               colorScheme="neutral"
               variant={"ghost"}
+              onClick={() =>
+                contentRef.current?.scrollIntoView({
+                  behavior: "smooth",
+                })
+              }
             />
           </Center>
         </VStack>
       </Box>
+      <Box bgColor="#121212" w="100vw" p={4}>
+        <VStack>
+          <HStack w="100%">
+            <Heading
+              textColor="subtle"
+              fontFamily={MonoFont.style.fontFamily}
+              size="2xl"
+            >
+              01
+            </Heading>
+            <Divider h="60px" orientation="vertical" />
+            <Heading
+              textColor="white"
+              fontFamily={MonoFont.style.fontFamily}
+              w="20%"
+            >
+              About
+            </Heading>
+            <Heading textColor="white" size="sm">
+              マイコン部について
+            </Heading>
+          </HStack>
+          <Text textColor="white">
+            本郷学園マイコン部は、東京都巣鴨に位置する中高一貫制の男子校、本郷学園の文化部です。
+            <br />
+            さまざまな興味関心を持つ部員たちが集い、日夜跳梁跋扈しています。
+          </Text>
+        </VStack>
+      </Box>
+      <Box bgColor="#121212" w="100vw" p={4}>
+        <VStack>
+          <HStack w="100%">
+            <Heading
+              textColor="subtle"
+              fontFamily={MonoFont.style.fontFamily}
+              size="2xl"
+            >
+              02
+            </Heading>
+            <Divider h="60px" orientation="vertical" />
+            <Heading
+              textColor="white"
+              fontFamily={MonoFont.style.fontFamily}
+              w="20%"
+            >
+              Activities
+            </Heading>
+            <Heading textColor="white" size="sm">
+              マイコン部の活動
+            </Heading>
+          </HStack>
+          <Text textColor="white">マイコン部の活動内容は、</Text>
+          <Center
+            bgImage="focus.svg"
+            bgSize="100%"
+            bgPosition="center"
+            bgRepeat="no-repeat"
+            h="200px"
+          >
+            <Heading textColor="white">
+              「およそ技術に関連していれば何をしてもよい」
+            </Heading>
+          </Center>
+          <Text textColor="white">です。</Text>
+        </VStack>
+      </Box>
+      <Box bgColor="#121212" h="100vh" w="100vw" p={4} id="dummy"></Box>
     </>
   );
 }
--- src/components/header/deskHeader.tsx
+++ src/components/header/deskHeader.tsx
@@ -1,4 +1,11 @@
-import { Box, HStack, IconButton, Image, Spacer } from "@yamada-ui/react";
+import {
+  Box,
+  Divider,
+  HStack,
+  IconButton,
+  Image,
+  Spacer,
+} from "@yamada-ui/react";
 import GithubIcon from "../githubIcon";
 import React from "react";
 import { useRouter } from "next/navigation";
@@ -6,36 +13,39 @@ import { useRouter } from "next/navigation";
 export default function DeskHeader() {
   const router = useRouter();
   return (
-    <Box bgColor="#121212" pos="sticky" h="72px" as="header" p={4}>
-      <HStack justifyContent="space-around">
-        <Image src="logo-w.png" alt="logo" h="40px" />
-        <Spacer />
-        <IconButton
-          variant="ghost"
-          icon={<GithubIcon />}
-          onClick={() => router.push("http://github.com/HongoMCC")}
-          size="md"
-          color="white"
-          colorScheme="neutral"
-        />
-        <IconButton
-          variant="ghost"
-          icon={
-            <Image
-              src="qiita-white-icon.png"
-              alt="qiita"
-              maxH="60%"
-              maxW="60%"
-            />
-          }
-          onClick={() =>
-            router.push("https://qiita.com/organizations/HongoMCC")
-          }
-          size="md"
-          color="white"
-          colorScheme="neutral"
-        />
-      </HStack>
+    <Box pos="sticky" h="72px" as="header" top={0} zIndex={1}>
+      <Box bgColor="#121212" p={4}>
+        <HStack justifyContent="space-around">
+          <Image src="logo-w.png" alt="logo" h="40px" />
+          <Spacer />
+          <IconButton
+            variant="ghost"
+            icon={<GithubIcon />}
+            onClick={() => router.push("http://github.com/HongoMCC")}
+            size="md"
+            color="white"
+            colorScheme="neutral"
+          />
+          <IconButton
+            variant="ghost"
+            icon={
+              <Image
+                src="qiita-white-icon.png"
+                alt="qiita"
+                maxH="60%"
+                maxW="60%"
+              />
+            }
+            onClick={() =>
+              router.push("https://qiita.com/organizations/HongoMCC")
+            }
+            size="md"
+            color="white"
+            colorScheme="neutral"
+          />
+        </HStack>
+      </Box>
+      <Divider />
     </Box>
   );
 }
